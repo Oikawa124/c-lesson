b2ca4262a974ccd7ab15eb4ba7b8f209309cbf8f
devel <oiku1515@gmail.com>
2018-11-25 14:02:29 +0900

    [add] スタック操作の練習　複数pop,push
    先にpopした中身だけ画面に表示される。

---
+++ sources/arm_asm/02_arm_assembly/2_5_print_mem/stack_practice2.s
@@ -0,0 +1,33 @@
+.globl _start
+_start:
+  ldr r1,=0x101f1000
+
+  mov r5, #0x31
+  mov r4, #0x34
+
+  ldr r13,=0x08000000
+
+  sub r13, r13, #1
+  str r5, [r13]
+
+  sub r13, r13, #1
+  str r4, [r13]
+
+  ldr r4, [r13]
+  add r13, r13, #1
+
+  ldr r5, [r13]
+  add r13, r13, #1
+
+  /* r4だけ表示される r5はスペース?なのか、文字がでない
+     先にpopしたほうだけ表示される。(r4とr5を逆にしても
+     r4だけ表示)*/
+
+  str r4, [r1]
+  str r5, [r1]
+
+
+  b busy
+
+busy:
+  b busy
