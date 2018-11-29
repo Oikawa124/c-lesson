a6a4141d18aa09bb3ece49dd16d09aa24b48a18d
devel <oiku1515@gmail.com>
2018-11-29 14:25:31 +0900

    [update] cl_get_result()を実装

--- sources/arm_asm/04_disasm/cl_utils.c
+++ sources/arm_asm/04_disasm/cl_utils.c
@@ -5,24 +5,39 @@
 
 static char buf[100*1024];
 
+static int buf_pos[1000];
+
 static int to_buffer = 0;
 static int pos = 0;
+static int pre_pos = 0;
+
+static int call_print_time = 0;
 
 void cl_enable_buffer_mode() {
     to_buffer = 1;
 }
 
 char *cl_get_printed_buffer() {
-    return buf;
+    return &buf[pre_pos];
+}
+
+char *cl_get_result(int num) {
+    return &buf[buf_pos[num-1]];
 }
 
 void cl_printf(char *fmt, ...) {
+    pre_pos = pos;
+
+
     va_list arg_ptr;
     va_start(arg_ptr, fmt);
 
     if(to_buffer) {
         pos += vsprintf(&buf[pos], fmt, arg_ptr);
         pos++;
+        buf_pos[call_print_time] = pos;
+        call_print_time++;
+
     } else {
         vprintf(fmt, arg_ptr);
     }
--- sources/arm_asm/04_disasm/main.c
+++ sources/arm_asm/04_disasm/main.c
@@ -41,7 +41,7 @@ int print_asm(int word) {
 
 int streq(char *s1, char *s2) { return 0 == strcmp(s1, s2); }
 
-static void test_print_asm_0xE3A01068() {
+static void test_print_asm_immediate68() {
     char *expect = "mov r1, #0x68";
     int input = 0xE3A01068;
 
@@ -54,7 +54,7 @@ static void test_print_asm_0xE3A01068() {
 }
 
 
-static void test_print_asm_0xE3A01065() {
+static void test_print_asm_immediate65() {
     char *expect = "mov r1, #0x65";
     int input = 0xE3A01065;
 
@@ -68,7 +68,7 @@ static void test_print_asm_0xE3A01065() {
 
 
 
-static void test_print_asm_EAFFFFFE() {
+static void test_print_asm_branch() {
     char *expect = "b [r15, #0x34]";
     int input = 0xEAFFFFFE;
 
@@ -81,7 +81,7 @@ static void test_print_asm_EAFFFFFE() {
 }
 
 
-static void test_print_asm_0x64646464() {
+static void test_print_asm_not_an_order() {
     int expect = '\0';
     int input = 0x64646464;
 
@@ -93,20 +93,34 @@ static void test_print_asm_0x64646464() {
     assert(actual[0] == expect);
 }
 
+static void test_print_asm_get_result() {
+    char *expect = "mov r1, #0x65";
+    int input1 = 0xE3A01065;
+    int input2 = 0xE3A01065;
+
+
+    print_asm(input1);
+    print_asm(input2);
+    char *actual = cl_get_result(1);
+
+    assert(streq(expect, actual));
+}
 
 static void unit_test() {
 
     cl_enable_buffer_mode();
 
     // 命令: mov
-    test_print_asm_0xE3A01068();
-    test_print_asm_0xE3A01065();
+    test_print_asm_immediate68();
+    test_print_asm_immediate65();
 
     // 命令: b
-    test_print_asm_EAFFFFFE();
+    test_print_asm_branch();
 
     //命令でない
-    test_print_asm_0x64646464();
+    test_print_asm_not_an_order();
+
+    test_print_asm_get_result();
 }
 
 
