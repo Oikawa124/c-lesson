9ca886ddb1079b4b61f787b2b76ccf8f9710033e
devel <oiku1515@gmail.com>
2019-01-17 23:07:20 +0900

    [add] regressionテストを追加

---
+++ sources/arm_asm/04_disasm/4_2_process_a_file/regression_test.ps1
@@ -0,0 +1,29 @@
+# Compile and run
+gcc -c .\cl_utils.c; gcc -c .\main.c
+gcc .\cl_utils.o .\main.o -o test
+
+.\test.exe > actual.txt
+
+# Read files
+$expect = Get-Content .\test\test_expect\expect.txt
+$actual = Get-Content actual.txt
+
+
+# Compare strings
+0..($expect.Count-1) | ForEach-Object{
+    $cmp = $expect[$_] -eq $actual[$_]
+
+    if (-Not($cmp))
+    {
+        Write-Host -NONewline  $expect[$_]' | '$actual[$_].Substring($actual[$_].Length -12, 12)
+        return # Not at "break"
+    }
+}
+
+# Delete file
+Remove-Item *.o
+Remove-Item test.exe
+Remove-Item actual.txt
+
+
+# print example >> 0x00010038  00 10 1f 11  |  00 10 1f 10
