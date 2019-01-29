7b52a89de90d9baa73db33ce039044aa18d50806
devel <oiku1515@gmail.com>
2019-01-29 21:11:49 +0900

    [update] parse_oneを実装・テストを追加

--- sources/arm_asm/05_asm/5_1_support_mov/NOTE.md
+++ sources/arm_asm/05_asm/5_1_support_mov/NOTE.md
@@ -11,3 +11,22 @@ ldrで=で大きな数字を読み込んだりはサポートしないが、
 
 mov r1, r2
 をアセンブルする。
+
+### mov r1, r2のobjdump結果
+
+0:   e1a01002        mov     r1, r2
+
+int expect = 0xE1A01002;
+
+
+### 1byte
+1byte = 8bit
+
+0001 1010 == 1A <- 1byte
+
+2 桁で 1 Byte
+
+
+### Unit Test用
+int streq(char *s1, char *s2) { return 0 == strcmp(s1, s2); }
+
--- sources/arm_asm/05_asm/5_1_support_mov/main.c
+++ sources/arm_asm/05_asm/5_1_support_mov/main.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
 #include <mem.h>
 #include <ctype.h>
+#include <assert.h>
 
 #define BUF_SIZE 100
+#define PRASE_FAIL -1
 
 
-enum TokenType {
-    mov,
-};
 
 struct substring {
     char *str;
     int len;
 };
-
-
-struct Token {
-    enum TokenType t_type;
-    struct substring sub;
-};
-
 
 
 // 1行読み込んだ文字列のバッファ
@@ -33,9 +25,10 @@ int cl_getline(char **out_buf, FILE *fp) {
     if ((ch = getc(fp)) == EOF) {return EOF;}
 
     do {
-        buf[len] = ch;
+        buf[len] = (char)ch;
         len++;
-    } while ((ch = getc(fp)) != '\n');
+
+    } while ((ch = getc(fp)) != '\n' && ch != EOF);
 
     buf[len] = '\0';
 
@@ -45,96 +38,214 @@ int cl_getline(char **out_buf, FILE *fp) {
 }
 
 
-// 先頭のシンボルを切り出す
-int parse_one(char *str, struct Token* out_token){
+// 先頭のシンボルを切り出す (ラベルや.rawは後で実装していく)
+int parse_one(int out_buf_pos, char *str, struct substring* out_sub_str){
 
-    int len = 0;
+    int str_pos = out_buf_pos;
     int ch;
-    char word_buf[BUF_SIZE];
 
+    if ((ch = str[str_pos]) == ':'
+         || ch == ',') {
+        out_sub_str->str = &str[out_buf_pos];
+        out_sub_str->len = 1;
+        str_pos++;
 
-    do {
-        ch = str[len];
-        word_buf[len] = (char)ch;
-
-        len++;
-
-    } while (!isspace(ch) && isalnum(ch));
-
-    word_buf[len] = '\0';
-
-
-    if (strcmp(word_buf, "mov")) {
-        out_token->t_type = mov;
-    } else {
-        printf("not complement");
+        return str_pos;
     }
 
-    out_token->sub.str = str;
-    out_token->sub.len = len-1;
+    // 空白スキップ
+    while (str[str_pos] == ' ') {
+        str_pos++;
+    }
 
-    return len;
+
+    int sub_str_first_pos = str_pos;
+
+    while ((ch = str[str_pos]) !='\0'
+            && ch != ' '
+            && ch != ':'
+            && ch != ',')
+    {
+        str_pos++;
+    }
+
+    out_sub_str->str = &str[sub_str_first_pos];
+    out_sub_str->len = str_pos - sub_str_first_pos;
+
+    return str_pos;
 }
 
 
-int parse_register(int read_len, char *str, int *out_register){
+int parse_register(int str_pos, char *str, int *out_register){
     int ch;
-    int len = read_len;
+    int pos = str_pos;
     int reg_mun = 0;
 
     // 空白・文字"r"読み飛ばし
     do {
-        ch = str[len];
-        len++;
+        ch = str[pos];
+        pos++;
     } while (isspace(ch) || isalpha(ch));
 
-    for (; isdigit(ch); ch = str[len]) {
+    // 数字取得
+    for (; isdigit(ch); ch = str[pos]) {
         reg_mun = reg_mun * 10 + (ch - '0');
-        len++;
+        pos++;
     }
 
-    *out_register = reg_mun;
+    if (0 <= reg_mun && reg_mun <= 15) {
+        *out_register = reg_mun;
+        return pos;
+    }
 
-    return len;
+    return PRASE_FAIL;
+}
+
+int skip_comma(int str_pos, char *str){
+
+    // コンマ読み飛ばし
+    int ch;
+    int pos = str_pos;
+
+    do {
+        ch = str[pos];
+        pos++;
+    } while (ch == ',');
+
+    if (str_pos < pos) {
+        return pos;
+    } else {
+        return PRASE_FAIL;
+    }
 }
 
 
 
-// 先頭のトークンを読み出して，結果によって分岐する
-void asm_one(FILE *fp){
 
+
+// 先頭のトークンを読み出して，結果によって分岐する
+int asm_one(FILE *fp){
 
     // 一行読み込み
     char *out_buf;
-    int len = cl_getline(&out_buf, fp);
+    int out_buf_len = 0;
+    int out_buf_pos = 0;
 
+
+    out_buf_len = cl_getline(&out_buf, fp);
 
 
     // 命令切り出し
-    struct Token symbol;
-
-    int read_len = parse_one(out_buf, &symbol);
+    struct substring sub_str;
 
 
-    // レジスタ切り出し
-    int reg_1st;
-
-    read_len = parse_register(read_len, out_buf, &reg_1st);
+    out_buf_pos = parse_one(out_buf_pos, out_buf, &sub_str);
 
 
+//    // レジスタ切り出し
+//    int reg_1st, reg_2nd;
+//
+//    out_buf_pos = parse_register(out_buf_pos, out_buf, &reg_1st);
+//
+//    if (out_buf_pos == PRASE_FAIL) { return out_buf_pos;}
+//
+//    out_buf_pos = skip_comma(out_buf_pos, out_buf);
+//
+//    if (out_buf_pos == PRASE_FAIL) { return out_buf_pos;}
+//
+//    out_buf_pos = parse_register(out_buf_pos, out_buf, &reg_2nd);
+//
+//    if (out_buf_pos == PRASE_FAIL) { return out_buf_pos;}
 
-    fclose(fp);
 
+    return 0;
+}
+
+
+
+
+/**************** Unit Tests ****************/
+
+
+int strneq(char *s1, char *s2, int len) { return 0 == strncmp(s1, s2, len); }
+
+void assert_substring_eq(char *expect, struct substring* actual){
+    assert(strneq(expect, actual->str, actual->len));
+}
+
+static void test_parse_one() {
+
+    char *input = "loop: mov r1, r2";
+    int input_pos = 0;
+
+    char *expect1 = "loop";
+    char *expect2 = ":";
+    char *expect3 = "mov";
+    char *expect4 = "r1";
+    char *expect5 = ",";
+    char *expect6 = "r2";
+
+    struct substring actual1;
+    struct substring actual2;
+    struct substring actual3;
+    struct substring actual4;
+    struct substring actual5;
+    struct substring actual6;
+
+
+    input_pos = parse_one(input_pos,input, &actual1);
+    input_pos = parse_one(input_pos,input, &actual2);
+    input_pos = parse_one(input_pos,input, &actual3);
+    input_pos = parse_one(input_pos,input, &actual4);
+    input_pos = parse_one(input_pos,input, &actual5);
+    input_pos = parse_one(input_pos,input, &actual6);
+
+    assert_substring_eq(expect1, &actual1);
+    assert_substring_eq(expect2, &actual2);
+    assert_substring_eq(expect3, &actual3);
+    assert_substring_eq(expect4, &actual4);
+    assert_substring_eq(expect5, &actual5);
+    assert_substring_eq(expect6, &actual6);
+}
+
+
+static void test_parse_register() {
+    char *input = " r1, r2";
+    int input_pos = 0;
+
+    int expect_reg_1 = 1;
+    int expect_reg_2 = 2;
+
+    int actual_reg_1, actual_reg_2;
+
+
+    input_pos = parse_register(input_pos, input, &actual_reg_1);
+
+    input_pos = skip_comma(input_pos, input);
+
+    input_pos = parse_register(input_pos, input, &actual_reg_2);
+
+    assert(expect_reg_1 == actual_reg_1);
+    assert(expect_reg_2 == actual_reg_2);
+}
+
+
+static void unit_tests() {
+
+    test_parse_one();
+    test_parse_register();
 }
 
 
 int main() {
 
-    FILE *fp = fopen("mov_op.txt", "r");
+    unit_tests();
 
-    asm_one(fp);
-
-    fclose(fp);
+//    FILE *fp = fopen("mov_op.ks", "r");
+//
+//    asm_one(fp);
+//
+//    fclose(fp);
 
     return 0;
 }
