ce5916f599580c73c076df980a2f5827822c0dcc
devel <oiku1515@gmail.com>
2019-03-22 21:54:08 +0900

    [add] 課題: アセンブリから文字列を渡して表示してみる　を実装

--- sources/arm_asm/06_c_function/call_c_msg/main.s
+++ sources/arm_asm/06_c_function/call_c_msg/main.s
@@ -1,7 +1,15 @@
 .globl _start
 _start:
     ldr r13,=0x07FFFFFF
-    // TODO: ここに何かを書いて、下のprint_msgで
+    ldr r0,=msg
+
+    // r0にmsgのアドレスを渡す
+
+    // print_msg:のアセンブリから
+    // str     r0, [sp, #4]
+    // となっている。
+    // r0のアドレスを [sp, #4]に渡す。
+
     // Hello Worldと表示するようにしてください。
     // 
     bl print_msg
