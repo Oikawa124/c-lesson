3fbc12b9e3790ab106107f76156f734971f769dc
devel <oiku1515@gmail.com>
2019-04-22 19:46:25 +0900

    [add] eval()の実装の途中まで

--- sources/casm_link/06_jit_ps/ps_jit.c
+++ sources/casm_link/06_jit_ps/ps_jit.c
@@ -7,7 +7,7 @@
 #include "parser.h"
 #include "test_util.h"
 
-extern int eval(int r0, int r1, char *str);
+///extern int eval(int r0, int r1, char *str);
 
 /*
 JIT
@@ -27,15 +27,144 @@ void ensure_jit_buf() {
 }
 
 
-// ここに実装していく
+/* ここに実装していく */
 
+
+// emitter バイナリをarrayに送るようにする。
+struct Emitter {
+    int *array;
+    int pos;
+};
+
+void initialize_result_arr(struct Emitter *emitter){
+    emitter->array = binary_buf;
+    emitter->pos = 0;
+}
+
+void emit_word(struct Emitter* emitter, unsigned int oneword){
+    emitter->array[emitter->pos] = oneword;
+    emitter->pos++;
+}
+
+
+
+// eval
+// ここを変えてバイナリを返すようにしていく。
+
+static int stack_pos = 0;
+static int stack[1024];
+void stack_push(int val) {
+    stack[stack_pos++] = val;
+}
+int stack_pop() {
+    if(stack_pos == 0) {
+        fprintf(stderr, "stack pop while stack is empty, exit.\n");
+        exit(1);
+    }
+    return stack[--stack_pos];
+}
+
+int eval(struct Emitter emitter, char *str) {
+    struct Substr remain={str, strlen(str)};
+    int val;
+
+    stack_pos = 0;
+
+    while(!is_end(&remain)) {
+        skip_space(&remain);
+        if(is_number(remain.ptr)) {
+
+            //stack_push(parse_number(remain.ptr));
+            // スタックにpushするバイナリ
+            emit_word(&emitter, 0xe3a02005); // mov r2, #5
+            emit_word(&emitter, 0xe52d2004); // push {r2}
+
+            skip_token(&remain);
+            //continue;
+        }
+
+//        }else if(is_register(remain.ptr)) {
+//            if(remain.ptr[1] == '1') {
+//                val = r1;
+//            } else {
+//                val = r0;
+//            }
+//            stack_push(val);
+//            skip_token(&remain);
+//            continue;
+//
+//        } else {
+//            // must be op.
+//            int arg1, arg2;
+//
+//            val = parse_word(&remain);
+//            skip_token(&remain);
+//
+//            arg2 = stack_pop();
+//            arg1 = stack_pop();
+//
+//            switch(val) {
+//                case OP_ADD:
+//                    stack_push(arg1+arg2);
+//                    break;
+//                case OP_SUB:
+//                    stack_push(arg1-arg2);
+//                    break;
+//                case OP_MUL:
+//                    stack_push(arg1*arg2);
+//                    break;
+//                case OP_DIV:
+//                    stack_push(arg1/arg2);
+//                    break;
+//            }
+//            continue;
+//        }
+    }
+
+    // return stack_pop();
+
+    // スタック(r2)をpopするバイナリ
+    emit_word(&emitter, 0xe49d2004);
+
+    // mov r0, r2するバイナリ
+    emit_word(&emitter, 0xe1a00002);
+
+    // mov, pc, lrするバイナリ
+    emit_word(&emitter, 0xe1a0f00e);
+
+
+    return 1;
+}
+
+
+
+
+
+
+// JITスクリプト
 int* jit_script(char *input) {
     ensure_jit_buf();
 
-    // emit binary in hard code
+    //　onewordを詰める配列を準備
+    struct Emitter emitter;
+    initialize_result_arr(&emitter);
 
-    binary_buf[0] = 0xe3a00005; // mov r0, #5
-    binary_buf[1] = 0xe1a0f00e; // mov r15, r14
+
+    int res = eval(emitter, input);
+
+    // emit binary in hard code
+//
+//    emit_word(&emitter, 0xe3a02005);
+//    emit_word(&emitter, 0xe52d2004);
+//    emit_word(&emitter, 0xe49d2004);
+//    emit_word(&emitter, 0xe1a00002);
+//    emit_word(&emitter, 0xe1a0f00e);
+
+//    binary_buf[0] = 0xe3a02005;
+//    binary_buf[1] = 0xe52d2004;
+//    binary_buf[2] = 0xe49d2004;
+//    binary_buf[3] = 0xe1a00002;
+//    binary_buf[4] = 0xe1a0f00e;
 
     return binary_buf;
 }
@@ -46,11 +175,11 @@ int* jit_script(char *input) {
 static void test_jit_script_input_number_5(){
 
     // SetUp
-    char *input = "5"; // r1だけスタックに残るを想定
+    char *input = "5"; // 5だけスタックに残るを想定
 
     int expect = 5;
 
-    int (*funcvar)();
+    int (*funcvar)(int, int);
     funcvar = (int(*)(int, int))jit_script(input);
 
     // Execute
@@ -58,8 +187,9 @@ static void test_jit_script_input_number_5(){
 
     // Verify
     assert(expect == actual);
-
 };
+
+
 
 
 
